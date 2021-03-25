4af60e547d67d632a164f4c90ee751ef979a5bfe
Thomas <thom.laffay@gmail.com>
2021-03-25 12:10:07 +0100

    Corrections

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jelly\PycharmProjects\CATS_SERVER\venv
+set VIRTUAL_ENV=C:\Users\Thomas\Documents\SQLKittyGame\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
